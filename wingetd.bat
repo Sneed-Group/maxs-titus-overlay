b11172fb1894dc3ab2fd420ea4d05d91ed713bc3
nodemixaholic <sam@sllord.info>
2024-02-26 21:24:03 +0000

    Upload files to ''

---
+++ wingetd.bat
@@ -0,0 +1,127 @@
+winget install "Upscayl.Upscayl"
+
+
+winget install "PrestonN.FreeTube"
+
+
+winget install "Audacity.Audacity"
+
+
+winget install "Bitdefender.Bitdefender"
+
+
+winget install "pizzaboxer.Bloxstrap"
+
+
+winget install "CalcProgrammer1.OpenRGB"
+
+
+winget install "GNU.Nano"
+
+
+winget install "Git.Git"
+
+
+winget install "Gyan.FFmpeg"
+
+
+winget install "IrfanSkiljan.IrfanView"
+
+
+winget install "KDE.Krita"
+
+
+winget install "LibreWolf.LibreWolf"
+
+
+winget install "Microsoft.EdgeWebView2Runtime"
+
+
+winget install "Microsoft.AppInstaller"
+
+
+winget install "Microsoft.UI.Xaml.2.7"
+
+
+winget install "Microsoft.UI.Xaml.2.8"
+
+
+winget install "Microsoft.VCLibs.Desktop.14"
+
+
+winget install "Microsoft.WindowsTerminal"
+
+
+winget install "Anaconda.Miniconda3"
+
+
+winget install "OBSProject.OBSStudio"
+
+
+winget install "Rufus.Rufus"
+
+
+winget install "Piriform.Speccy"
+
+
+winget install "SumatraPDF.SumatraPDF"
+
+
+winget install "Ventoy.Ventoy"
+
+
+winget install "Ferdium.Ferdium"
+
+
+winget install "Balena.Etcher"
+
+
+winget install "KDE.Kdenlive"
+
+
+winget install "Roblox.Roblox"
+
+
+winget install "yt-dlp.yt-dlp"
+
+
+winget install "7zip.7zip"
+
+
+winget install "Microsoft.VCRedist.2015+.x86"
+
+
+winget install "TheDocumentFoundation.LibreOffice"
+
+
+winget install "Microsoft.DotNet.DesktopRuntime.7"
+
+
+winget install "VideoLAN.VLC"
+
+
+winget install "BlenderFoundation.Blender"
+
+
+winget install "EpicGames.EpicGamesLauncher"
+
+
+winget install "Valve.Steam"
+
+
+winget install "OpenJS.NodeJS.LTS"
+
+
+winget install "rocksdanister.LivelyWallpaper"
+
+
+winget install "Microsoft.VisualStudioCode"
+
+
+winget install "Microsoft.VCRedist.2015+.x64"
+
+
+winget install "Microsoft.DotNet.DesktopRuntime.7"
+
+
+winget install "Microsoft.DotNet.DesktopRuntime.7"
