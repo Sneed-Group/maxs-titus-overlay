9491c3524be61218d6932d68ffee20122546b8f8
nodemixaholic <sam@sllord.info>
2024-03-13 18:43:26 +0000

    Add 'debloat-portable.ps1'

---
+++ debloat-portable.ps1
@@ -0,0 +1,21 @@
+echo Stopping and Disabling Windows Update services...
+Stop-Service -Name wuauserv
+Set-Service -Name wuauserv -StartupType Disabled
+Stop-Service -Name bits
+Set-Service -Name bits -StartupType Disabled
+Stop-Service -Name dosvc
+Set-Service -Name dosvc -StartupType Disabled
+
+echo Setting region to Denmark....
+PowerShell Set-WinHomeLocation -GeoID 61
+
+echo Removing bloat...
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name BingSearchEnabled -Value 0
+irm "https://github.com/ChrisTitusTech/winutil/raw/main/edgeremoval.ps1" | iex
+powershell -command 'irm "http://nodemixaholic.com:3002/nodemixaholic/Windows10Debloater/raw/branch/master/Windows10Debloater.ps1" | iex
+Get-AppxPackage -alluser *Paint* | Remove-Appxpackage
+Get-AppxPackage -alluser *Notepad* | Remove-Appxpackage
+Get-AppxPackage -alluser *DevHome* | Remove-Appxpackage
+et-AppxPackage -alluser *YourPhone* | Remove-Appxpackage
+Get-AppxPackage -alluser *WindowsStore* | Remove-Appxpackage
+irm "http://nodemixaholic.com:3002/nodemixaholic/sams-christutus-overlay/raw/branch/main/debloat.ps1" | iex
