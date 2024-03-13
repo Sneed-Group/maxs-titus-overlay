811443f5678f39dc3cf752bb8377e5fa486b778e
nodemixaholic <sam@sllord.info>
2024-03-13 19:08:32 +0000

    Update 'desktop.bat'

--- desktop.bat
+++ desktop.bat
@@ -4,7 +4,7 @@ color 0a
 title [Desktop] Sams Chris Titus Overlay
 
 echo Doing common stuff...
-common.bat
+powershell -command "irm | 'http://nodemixaholic.com:3002/nodemixaholic/sams-christutus-overlay/raw/branch/main/common-stream.ps1' iex"
 
 echo Installing apps fron winget config file....
 wingetd.bat
