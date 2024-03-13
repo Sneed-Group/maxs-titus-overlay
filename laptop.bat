d9a16651889b8b75f4063c144861b7420e055984
nodemixaholic <sam@sllord.info>
2024-03-13 18:05:32 +0000

    Update 'laptop.bat'

--- laptop.bat
+++ laptop.bat
@@ -6,8 +6,6 @@ title [Laptop] Sams Chris Titus Overlay
 echo Installing apps fron winget config file....
 wingetl.bat
 
-echo Running Shutup 10....
-oosu10.exe shutmouth.cfg /quiet
 
 echo Stopping and Disabling Windows Update services...
 net stop wuauserv
@@ -20,12 +18,7 @@ sc config dosvc start= disabled
 echo Setting region to Denmark....
 PowerShell Set-WinHomeLocation -GeoID 61
 
-echo Removing stuff CTT Utility missed....
-Get-AppxPackage -alluser *Paint* | Remove-Appxpackage
-Get-AppxPackage -alluser *Notepad* | Remove-Appxpackage
-Get-AppxPackage -alluser *DevHome* | Remove-Appxpackage
-Get-AppxPackage -alluser *YourPhone* | Remove-Appxpackage
-Get-AppxPackage -alluser *WindowsStore* | Remove-Appxpackage
+common.bat
 
 echo Done.
 pause
