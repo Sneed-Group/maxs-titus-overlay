1c507bfe08554bdcd87779868f10f3f706064e74
Sam Sneed <163201376+sam-sneed@users.noreply.github.com>
2024-07-15 15:42:10 -0500

    Update common-stream.ps1

--- common-stream.ps1
+++ common-stream.ps1
@@ -24,15 +24,15 @@ Set-WinHomeLocation -GeoID 61
 echo Removing bloat...
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name BingSearchEnabled -Value 0
 irm "https://github.com/ChrisTitusTech/winutil/raw/main/edgeremoval.ps1" | iex
-irm "http://nodemixaholic.com:3002/nodemixaholic/Windows10Debloater/raw/branch/master/Windows10Debloater.ps1" | iex
+irm "https://github.com/Sneed-Group/Windows10Debloater/raw/master/Windows10Debloater.ps1" | iex
 Get-AppxPackage -alluser *Paint* | Remove-Appxpackage
 Get-AppxPackage -alluser *Notepad* | Remove-Appxpackage
 Get-AppxPackage -alluser *DevHome* | Remove-Appxpackage
 et-AppxPackage -alluser *YourPhone* | Remove-Appxpackage
 Get-AppxPackage -alluser *WindowsStore* | Remove-Appxpackage
-irm "http://nodemixaholic.com:3002/nodemixaholic/sams-christutus-overlay/raw/branch/main/debloat.ps1" | iex
+irm "https://github.com/Sneed-Group/maxs-titus-overlay/raw/main/debloat.ps1" | iex
 
-Invoke-WebRequest -Uri "http://nodemixaholic.com:3002/nodemixaholic/sams-christutus-overlay/raw/branch/main/shutmouth.cfg" -OutFile "shutmouth.cfg"
+Invoke-WebRequest -Uri "https://github.com/Sneed-Group/maxs-titus-overlay/raw/main/shutmouth.cfg" -OutFile "shutmouth.cfg"
 Invoke-WebRequest -Uri "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -OutFile "oosu10.exe"
 irm "http://nodemixaholic.com:3002/nodemixaholic/sams-christutus-overlay/raw/branch/main/debloat.ps1" | iex
 ./oosu10.exe shutmouth.cfg /quiet
